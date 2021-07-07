081cda5919bda092dadfb89d191ab05aec364511
shaibal <shaibal58@gmail.com>
2021-07-07 10:44:20 -0400

    edit sout for HW2

--- src/main/java/HomeWork/HW2.java
+++ src/main/java/HomeWork/HW2.java
@@ -3,7 +3,8 @@ package HomeWork;
 public class HW2 {
     public static void main(String[] args) {
         System.out.println();
-        System.out.println("Coder");
+        System.out.println("BestCoder");
+        System.out.println("1st code Push");
 
     }
 }
