50ca78605e4cd8eaedfd3a125dafc0db4382dbc8
shaibal <shaibal58@gmail.com>
2021-07-15 11:19:18 -0400

    AddingHW3

---
+++ src/main/java/Class2/JavaOperators.java
@@ -0,0 +1,15 @@
+package Class2;
+
+public class JavaOperators
+{
+    /**
+     * Java Operators:
+     * 1. Assignment Operator (=)
+     * 2. Arithmetic (+, -, *, /, %, ++, --)
+     * 3. Comparison (>, >=, <, <=, ==, !=)
+     * 4. Logical (&&, ||, !)
+     * 5. Ternary (?:)
+     * 6. Advanced Assignment (+=, -=, *=, /=, %=)
+     */
+
+}
