38191739b803cc04a8826e0fe216943dea0169e9
shaibal <shaibal58@gmail.com>
2021-09-19 14:01:35 -0400

    adding git commands

--- src/main/java/Class4/HW3.java
+++ src/main/java/Class4/HW3.java
@@ -6,6 +6,7 @@ import javax.imageio.ImageReader;
 import java.sql.Struct;
 import java.util.Scanner;
 
+
 public class HW3
 {
     public static void main(String[] args)
@@ -68,14 +69,17 @@ public class HW3
 //        boolean doesSentence3StartWithk = sentence_3.equalsIgnoreCase("k");
 //        System.out.println("\nC) Does Sentence3 end with 'k'? " +sentence_3.endsWith("k"));
 //
-        String doesSentence3StartsWith = (sentence_3.startsWith("a") ? "Media" : "Entertainment");
-        System.out.println("result_3 = " + doesSentence3StartsWith);
+//        String doesSentence3StartsWith = (sentence_3.startsWith("a") ? "Media" : "Entertainment");
+//        System.out.println("result_3 = " + doesSentence3StartsWith);
 
 //        int result_1 = sentence_1.length() > 10 ? 100 : 15;
 //
 //        System.out.println("result_1 = " + result_1);
 //
 //
+
+
+
 
         /**
          * Assign result_1 as 100 if the length of sentence_1 is 10 or greater
@@ -93,6 +97,39 @@ public class HW3
         int result_1 = sentence_1.length() > 10 ? 100 : 15;
 
         System.out.println("result_1 = " + result_1);*/
+
+
+//        // Question - 1
+//        String sentence_1 = "hello dear, how are you?";
+//        int result_1 = 0;
+//        /**
+//         * Assign result_1 as 100 if the length of sentence_1 is 10 or greater
+//         * else assign result_1 as 15
+//         *
+//         * use: Ternary operator
+//         *
+//         */
+//        System.out.println("result_1 = " + result_1);
+//
+//        // Question - 2
+//        String sentence_2 = "We all ARe GOod ProgrAMmer";
+//        /**
+//         * replace all 'R/r' with 'F'
+//         */
+//        System.out.println(sentence_2);     // "We all AFe GOod PFogFAMmeF";
+//
+//        // Question - 3
+//        String sentence_3 = "ABC Networks";
+//        String result_3 = "";
+//        /**
+//         * 1. Display/Print length of your sentence_3
+//         * 2. does sentence_3 starts with "a" (ignore cases)
+//         * 3. does sentence_3 ends with "K" (ignore cases)
+//         * 4. Assign result_3 as "Media" if sentence_3 starts with 'a' (ignore cases)
+//         *      else assign "Entertainment"
+//         *      Print the result_3 in console.
+//         */
+
 
 
 
